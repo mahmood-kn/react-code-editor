320009417fd44cb28adb95d8c537b5aee81deca3
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-12-04 16:38:06 +0330

    add editor

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,66 @@
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
+import './CodeEditor.css';
+
+interface Props {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor = ({ initialValue, onChange }: Props) => {
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
+
+    editorRef.current.setValue(formatted);
+  };
+  return (
+    <div className='editor-wrapper'>
+      <button
+        onClick={onFormatClick}
+        className='button button-format is-primary is-small'>
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        language='javascript'
+        theme='dark'
+        height='500px'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
