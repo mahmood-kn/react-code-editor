6b3cd5c26593cb5fb34261059407819b17f222b8
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-12-06 17:33:41 +0330

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // import CodeCell from './components/CodeCell';
 import TextEditor from './components/TextEditor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
